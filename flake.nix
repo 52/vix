ccf1f5738e3357430a590046069fef94a3587d11
Max Karou <maxkarou@protonmail.com>
2025-08-31 22:17:42 +0200

    docs: format comment

--- flake.nix
+++ flake.nix
@@ -71,7 +71,8 @@
         };
       in
       {
-        # Shell used by 'nix develop', see: https://nix.dev/manual/nix/2.17/command-ref/new-cli/nix3-develop
+        # Shell used by "nix develop".
+        # See: https://nix.dev/manual/nix/2.18/command-ref/new-cli/nix3-develop
         devShell = pkgs.mkShell {
           buildInputs = attrValues {
             inherit (pkgs)
